d8fa6a8223a92c49c590965d8d4d02a980069e21
wangq <wangquanze922@gmail.com>
2024-04-02 15:40:43 -0500

    initial commit

---
+++ frontend/airbnb/src/utils.js
@@ -0,0 +1,5 @@
+const domin = "https://airbnb-418820.uc.r.appspot.com";
+
+export const login = (credential, asHost) => {
+    const loginUrl = '${domain}/authenticate/${asHost ? "host" : "guest"}';
+}
